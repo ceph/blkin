d161c89484f7a7b6724f078231dc84612cdb1278
Casey Bodley <casey@cohortfs.com>
2014-12-04 15:30:35 -0500

    blkin: allow delayed Trace initialization
    to make Trace easier to use on the stack and compose within data
    structures, allow them to be default-constructed and initialized later
    
    new function valid() and accompanying operator bool() return false
    on default-constructed Traces until an init() function is called
    
    name and ip strings are no longer copied into std::string by default,
    under the assumption that string literals will be used in the majority
    of cases.  new copy_ip() and copy_name() functions are available to
    handle cases where temporary strings are involved, and need to be stored
    for the lifetime of Trace
    
    Signed-off-by: Casey Bodley <casey@cohortfs.com>

--- blkin-lib/ztracer.hpp
+++ blkin-lib/ztracer.hpp
@@ -40,84 +40,158 @@ extern "C" {
 namespace ZTracer {
 	using std::string;
 
-	int ztrace_init() { return blkin_init(); }
+	static inline int ztrace_init() { return blkin_init(); }
 
 	class Endpoint : private blkin_endpoint {
 		private:
-			string _ip;
-			string _name;
+			string _ip; // storage for blkin_endpoint.ip, see copy_ip()
+			string _name; // storage for blkin_endpoint.name, see copy_name()
 
 			friend class Trace;
 		public:
-			Endpoint(const string &ip, int port, const string &name)
-				: _ip(ip), _name(name)
+			Endpoint(const char *ip, int port, const char *name)
 			{
-				blkin_init_endpoint(this, ip.c_str(), port, name.c_str());
+				blkin_init_endpoint(this, ip, port, name);
 			}
+
+      // Endpoint assumes that ip and name will be string literals, and
+      // avoids making a copy and freeing on destruction.  if you need to
+      // initialize Endpoint with a temporary string, copy_ip/copy_name()
+      // will store it in a std::string and assign from that
+      void copy_ip(const string &newip)
+      {
+        _ip.assign(newip);
+        ip = _ip.c_str();
+      }
+      void copy_name(const string &newname)
+      {
+        _name.assign(newname);
+        name = _name.c_str();
+      }
+
+      void set_port(int p) { port = p; }
 	};
 
 	class Trace : private blkin_trace {
-		private:
-			string _name;
+    private:
+      string _name; // storage for blkin_trace.name, see copy_name()
 
 		public:
-			Trace(const string &name, Endpoint *ep)
-				: _name(name)
-			{
-				blkin_init_new_trace(this, _name.c_str(), ep);
-			}
+      // default constructor zero-initializes blkin_trace
+      // valid() will return false on a default-constructed Trace until init()
+      Trace()
+      {
+        // zero-initialize so valid() returns false
+        name = NULL;
+        info.trace_id = 0;
+        info.span_id = 0;
+        info.parent_span_id = 0;
+        endpoint = NULL;
+      }
 
-			Trace(const string &name, Trace *parent)
-				: _name(name)
-			{
-				blkin_init_child(this, parent, parent->endpoint, _name.c_str());
-			}
+      // construct a Trace with an optional parent
+			Trace(const char *name, const Endpoint *ep, const Trace *parent = NULL)
+      {
+        if (parent && parent->valid()) {
+          blkin_init_child(this, parent, ep ? : parent->endpoint, name);
+        } else {
+				  blkin_init_new_trace(this, name, ep);
+        }
+      }
 
-			Trace(const string &name, Trace *parent, Endpoint *ep)
-				: _name(name)
-			{
-				blkin_init_child(this, parent, ep, _name.c_str());
-			}
-
-			Trace(const string &name, Endpoint *ep,
+      // construct a Trace from blkin_trace_info
+			Trace(const char *name, const Endpoint *ep,
             const blkin_trace_info *i, bool child=false)
-				: _name(name)
 			{
 				if (child)
-					blkin_init_child_info(this, i, ep, _name.c_str());
+					blkin_init_child_info(this, i, ep, name);
 				else {
-					blkin_init_new_trace(this, _name.c_str(), ep);
+					blkin_init_new_trace(this, name, ep);
 					set_info(i);
 				}
 			}
 
+      Trace(const Trace &rhs) : _name(rhs._name)
+      {
+        if (rhs.name == rhs._name.c_str())
+          name = _name.c_str();
+        else
+          name = rhs.name;
+        info = rhs.info;
+        endpoint = rhs.endpoint;
+      }
+
+      // return true if the Trace has been initialized
+      bool valid() const { return info.trace_id != 0; }
+      operator bool() const { return valid(); }
+
+      // (re)initialize a Trace with an optional parent
+			int init(const char *name, const Endpoint *ep, const Trace *parent = NULL)
+      {
+        if (parent && parent->valid())
+          return blkin_init_child(this, parent, ep ? : parent->endpoint, name);
+
+        return blkin_init_new_trace(this, name, ep);
+      }
+
+      // (re)initialize a Trace from blkin_trace_info
+			int init(const char *name, const Endpoint *ep,
+               const blkin_trace_info *i, bool child=false)
+      {
+				if (child)
+					return blkin_init_child_info(this, i, ep, _name.c_str());
+
+        int r = blkin_init_new_trace(this, _name.c_str(), ep);
+        if (r == 0)
+          set_info(i);
+        return r;
+      }
+
+      // Trace assumes that name will be a string literal, and avoids
+      // making a copy and freeing on destruction.  if you need to
+      // initialize Trace with a temporary string, copy_name() will store
+      // it in a std::string and assign from that
+      void copy_name(const string &newname)
+      {
+        _name.assign(newname);
+        name = _name.c_str();
+      }
+
       const blkin_trace_info* get_info() const { return &info; }
       void set_info(const blkin_trace_info *i) { info = *i; }
 
+      // record key-value annotations
 			void keyval(const char *key, const char *val)
 			{
-				BLKIN_KEYVAL_STRING(this, endpoint, key, val);
+        if (valid())
+          BLKIN_KEYVAL_STRING(this, endpoint, key, val);
 			}
 			void keyval(const char *key, int64_t val)
 			{
-				BLKIN_KEYVAL_INTEGER(this, endpoint, key, val);
+        if (valid())
+          BLKIN_KEYVAL_INTEGER(this, endpoint, key, val);
 			}
 			void keyval(const char *key, const char *val, const Endpoint *ep)
 			{
-				BLKIN_KEYVAL_STRING(this, ep, key, val);
+        if (valid())
+          BLKIN_KEYVAL_STRING(this, ep, key, val);
 			}
 			void keyval(const char *key, int64_t val, const Endpoint *ep)
 			{
-				BLKIN_KEYVAL_INTEGER(this, ep, key, val);
+        if (valid())
+          BLKIN_KEYVAL_INTEGER(this, ep, key, val);
 			}
 
+      // record timestamp annotations
 			void event(const char *event)
 			{
-				BLKIN_TIMESTAMP(this, endpoint, event);
+        if (valid())
+          BLKIN_TIMESTAMP(this, endpoint, event);
 			}
 			void event(const char *event, const Endpoint *ep)
 			{
-				BLKIN_TIMESTAMP(this, ep, event);
+        if (valid())
+          BLKIN_TIMESTAMP(this, ep, event);
 			}
 	};
 
